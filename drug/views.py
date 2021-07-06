6818bde5bc3b17a0e0aa78729e804eac2fa7320d
Omar <omardraz94@gmail.com>
2021-07-05 21:39:00 -0700

    all done

--- drug/views.py
+++ drug/views.py
@@ -10,3 +10,8 @@ from rest_framework import generics
 class UserListCreate(generics.ListCreateAPIView):
     queryset = Drug.objects.all()
     serializer_class = DrugSerializer
+
+    def list(self, reuqest):
+        queryset = self.get_queryset()
+        serializer = DrugSerializer(queryset, many=True)
+        return Response(serializer.data)

d730dfef7a149789a6c186a6b5f866ef1a235f53
Omar <omardraz94@gmail.com>
2021-07-07 17:53:00 -0700

    small push, still need to figure htis out! it will take some time but I'll be better fori t

--- drug/models.py
+++ drug/models.py
@@ -11,21 +11,46 @@ class Drug(models.Model):
     packaging = models.ForeignKey('drug.Packaging', on_delete=models.CASCADE, default='', related_name='+')
     listing_expiration_date = models.DateField()
     openfda = models.ForeignKey('drug.OpenFDA', on_delete=models.CASCADE, default='', related_name='+')
+    marketing_category = models.CharField(max_length=100)
+    dosage_form = models.CharField(max_length=100)
+    spl_id = models.CharField(max_length=100)
+    product_type = models.CharField(max_length=100)
+    route = models.CharField(max_length=100)
+    marketing_start_date = models.DateField()
+    product_id = models.CharField(max_length=100)
+    application_number = models.CharField(max_length=100)
+    brand_name_base = models.CharField(max_length=100)
+    pharm_class = models.CharField(max_length=100)
 
 class ActiveIngredients(models.Model):
-    drug = models.ForeignKey(Drug, on_delete=models.CASCADE, default='', related_name='+')    
+    drug = models.ForeignKey(
+        Drug,
+        to_field='active_ingredients',
+        on_delete=models.CASCADE,
+        related_name='+'
+    )
     name = models.CharField(max_length=100)
     strength = models.CharField(max_length=100)
 
 class Packaging(models.Model):
-    drug = models.ForeignKey(Drug, on_delete=models.CASCADE, default='', related_name='+')   
+    drug = models.ForeignKey(
+        Drug,
+        to_field='packaging',
+        on_delete=models.CASCADE,
+        related_name='+'
+    )
     package_ndc = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     marketing_start_date = models.DateField()
     sample = models.BooleanField()
 
 class OpenFDA(models.Model):
-    drug = models.ForeignKey(Drug, on_delete=models.CASCADE, default='', related_name='+')
+    drug = models.ForeignKey(
+        Drug,
+        to_field='openfda',
+        on_delete=models.CASCADE,
+        related_name='+'
+    )
     manufacturer_name = models.CharField(max_length=100)
     rxcui = models.CharField(max_length=100)
     unii = models.CharField(max_length=100)

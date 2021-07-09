1a6d326d9ea5adbe38d2d87fd294b407c88d1593
Omar <omardraz94@gmail.com>
2021-07-08 20:03:40 -0700

    Smal change

--- drug/serializers.py
+++ drug/serializers.py
@@ -2,9 +2,22 @@ from .models import Drug, Packaging, ActiveIngredients, OpenFDA
 from rest_framework import serializers 
 from .utils import get_if_exists 
 
+class PackagingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drug
+        fields = '__all__'
+
+class ActiveIngredientsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drug
+        fields = '__all__'
+
+class OpenFDASerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drug
+        fields = '__all__'
 
 class DrugSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Drug
         fields = '__all__'

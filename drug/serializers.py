db4e2aa936a36ce68805fa3f13aeee5c277aa965
Omar <omardraz94@gmail.com>
2021-07-24 17:22:16 -0700

    small update

--- drug/serializers.py
+++ drug/serializers.py
@@ -14,6 +14,9 @@ class ActiveIngredientsSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class OpenFDASerializer(serializers.ModelSerializer):
+    
+
+
     class Meta:
         model = OpenFDA
         fields = '__all__'
@@ -32,10 +35,20 @@ class DrugSerializer(serializers.ModelSerializer):
             allow_empty=True
         )
     
-    openfda_data = serializers.ListField(
-            child=serializers.CharField(),
-            allow_empty=True
-        )
+    def create(self, validated_data):
+        packaging_data = validated_data.pop('packaging')
+        active_ingredients_data = validated_data.pop('active_ingredients')
+
+
+        drug = Drug.objects.create(**validated_data)
+
+        # for openfda_datas in openfda_data:
+        #     OpenFDA.objects.create(drug=drug, **openfda_datas)
+        for packaging_datas in packaging_data:
+            Packaging.objects.create(drug=drug, **packaging_datas)
+        for active_ingredients_datas in active_ingredients_data:
+            ActiveIngredients.objects.create(drug=drug, **active_ingredients_data)
+        return drug
 
 
 

d7c76bd78424aa62ce4138745ae273eb4993e878
Omar <omardraz94@gmail.com>
2021-07-30 22:13:24 -0700

    last commit - will pickup later

--- drug/views.py
+++ drug/views.py
@@ -5,6 +5,8 @@ from .models import Drug
 from django.http import Http404
 from .serializers import DrugSerializer
 from rest_framework import status
+from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 class DrugList(generics.ListCreateAPIView):
     queryset = Drug.objects.all()
@@ -15,4 +17,10 @@ class DrugDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Drug.objects.all()
     serializer_class = DrugSerializer
 
+def upload(request):
+    if request.method == "POST":
+        uploaded_file = request.FILES['document']
+        print(uploaded_file.name)
+        print(uploaded_file.size)
+    return render(request, 'upload.html')
     
